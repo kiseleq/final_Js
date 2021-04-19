dfd0ba31d23fadd58dfc860ce8e8e069ac3bf52d
Kiselev Andrey <kiseleq@gmail.com>
2021-04-19 17:36:48 +0300

    slider

---
+++ script/script.js
@@ -0,0 +1,31 @@
+window.addEventListener('DOMContentLoaded', function(){
+    'use strict';
+
+    //Slider
+    const slider = () => {
+        const slide = document.querySelectorAll('.item');
+        console.log(slide);
+
+        let currentSlide = 0;
+
+        let autoPlaySlide = () => {
+             
+            slide[currentSlide].style.display = 'none';
+            
+            currentSlide++;
+            if (currentSlide === 3) currentSlide = 0;
+            slide[currentSlide].style.display = 'block';
+
+        };
+
+        let startSlide = () => {
+            setInterval(autoPlaySlide, 3000);
+
+        };
+
+        startSlide();
+
+    };
+
+    slider();
+});
