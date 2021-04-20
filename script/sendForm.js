1f8fbe22f7fa48515efae7cb60cd02d558bb3ef2
Kiselev Andrey <kiseleq@gmail.com>
2021-04-20 17:28:52 +0300

    dop

---
+++ script/sendForm.js
@@ -0,0 +1,17 @@
+window.addEventListener('DOMContentLoaded', function(){
+    'use strict';
+    
+    const sendForm = () => {
+        const errorMessage = 'Что-то пошло не так...',
+            loadMessage = 'Загрузка...',
+            successMessage = 'Спасибо! Мы скоро с вами свяжемся!';
+
+        const submitBtn = document.querySelector('.submit-btn');
+        console.log(submitBtn);
+
+
+    };
+
+    sendForm();
+
+});
