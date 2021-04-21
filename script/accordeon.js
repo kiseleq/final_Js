7f61312d8e43da66ed36a8de88eea6622d0004fb
Kiselev Andrey <kiseleq@gmail.com>
2021-04-21 12:53:51 +0300

    accord

---
+++ script/accordeon.js
@@ -0,0 +1,35 @@
+window.addEventListener('DOMContentLoaded', function(){
+    'use strict';
+
+    const accordeon = () => {
+        const accord = document.querySelectorAll('.accord'),
+            del = document.querySelector('.del');
+            del.style.display = 'none';
+
+        accord.forEach((item, index) => {
+            item.addEventListener('click', () => {
+                if (item.classList.contains('active')) {
+                    item.classList.remove('active');
+                    const child = item.querySelector('.element-content');
+                    child.style.display = 'none';
+                } else {
+                    accord.forEach((item) => {
+                        item.classList.remove('active');
+                        const child = item.querySelector('.element-content');
+                        child.style.display = 'none';
+                    });
+
+                    item.classList.add('active');
+                    const child = item.querySelector('.element-content');
+                    child.style.display = 'block';
+                }
+            });
+        });
+
+
+    };
+
+    accordeon();
+
+
+});
--- script/sendForm.js
+++ script/sendForm.js
@@ -7,7 +7,17 @@ window.addEventListener('DOMContentLoaded', function(){
             successMessage = 'Спасибо! Мы скоро с вами свяжемся!';
 
         const submitBtn = document.querySelector('.submit-btn');
-        console.log(submitBtn);
+         
+        submitBtn.addEventListener('click', (event) => {
+            event.preventDefault();
+
+            fetch('./server.php')
+                .then((response) => {
+                    console.log(response);
+                });
+                
+
+        });
 
 
     };
