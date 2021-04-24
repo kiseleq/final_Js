c450f52f1ccf7a0159b61c7f0558b394a823c966
Kiselev Andrey <kiseleq@gmail.com>
2021-04-24 21:55:09 +0300

    promise

--- script/sendForm.js
+++ script/sendForm.js
@@ -6,17 +6,72 @@ window.addEventListener('DOMContentLoaded', function(){
             loadMessage = 'Загрузка...',
             successMessage = 'Спасибо! Мы скоро с вами свяжемся!';
 
-        const submitBtn = document.querySelector('.submit-btn');
+        const submitBtn = document.querySelector('.submit-btn'),
+            modalWindow = document.querySelector('.modalWindow'),
+            nameForm = document.querySelector('.name-form'),
+            phoneForm = document.querySelector('.phone-form');
          
         submitBtn.addEventListener('click', (event) => {
             event.preventDefault();
+            if (nameForm.value != '' && phoneForm.value != '') {
+                const createMessage = () => {
+                    modalWindow.insertAdjacentHTML('beforeend', `
+                    <div class='modal-message delete5'></div>
+                    `);
+                    const modalMessage = document.querySelector('.modal-message');
+                    modalMessage.textContent = loadMessage;
+                    modalMessage.style.textContent = `
+                        font-size: 3rem;
+                        margin: auto;
+                        font-family: "Open Sans", sans-serif;
 
-            fetch('./server.php')
-                .then((response) => {
-                    console.log(response);
-                });
-                
+                    `;
 
+                    nameForm.value = '';
+                    phoneForm.value = '';
+    
+                };
+                createMessage();
+    
+                const deleteMessage = () => {
+                    const modalMessage = document.querySelector('.modal-message');
+                    modalMessage.remove();
+                    
+                };
+                setTimeout(deleteMessage, 5000);
+
+                const modalMessage = document.querySelector('.modal-message');
+
+                const send = () => {
+                    const body = {
+                        name: nameForm.value,
+                        phone: phoneForm.value,
+                    };
+                    return fetch('server.php', {
+                        headers: {
+                            'Content-Type': 'application/json',
+                        },
+                        method: 'post',
+                        body: JSON.stringify(body),
+                    });
+
+                };
+
+                send()
+                    .then(() => {
+                        modalMessage.textContent = successMessage;
+                    })
+                    .catch( (err) => {
+                        modalMessage.textContent = errorMessage;
+                    });
+
+
+
+            } else {
+                alert('Введите данные в поля ввода!');
+            }
+            
+           
         });
 
 
